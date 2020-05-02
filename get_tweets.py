c2e89d6a364bb0689ca1fc874937f2863b6ef96a
NuttySalmon <nuttysalmon@gmail.com>
2020-05-02 00:28:36 -0700

    Added multithreaded operation.

--- get_tweets.py
+++ get_tweets.py
@@ -1,21 +1,39 @@
 import GetOldTweets3 as got
+import concurrent.futures
 import csv
-tweetCriteria = got.manager.TweetCriteria().setUsername("elonmusk")\
-                                           .setSince("2010-04-26")\
-                                           .setUntil("2020-04-27")\
-                                           .setMaxTweets(10000)\
-                                           .setEmoji("none")
+from datetime import datetime, timedelta
 
-print('Getting Tweets...')
-tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-if len(tweets) <= 0:
+def getTweets(since, until):
+    tweetCriteria = got.manager.TweetCriteria().setUsername("elonmusk")\
+                                            .setSince(since)\
+                                            .setUntil(until)\
+                                            .setMaxTweets(5000)\
+                                            .setEmoji("none")
+
+    print('Getting Tweets {} to {}'.format(since, until))
+    return got.manager.TweetManager.getTweets(tweetCriteria)
+
+tweets = list()
+dates = [["2020-04-26", "2020-04-27"], ["2020-04-26", "2020-04-27"], ["2020-04-26", "2020-04-27"]]
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    # Start the load operations and mark each future with its URL
+    zipped_pairs = zip(*dates)
+    starts, ends = zipped_pairs
+    results = executor.map(getTweets, starts, ends)
+    for result in results:
+        tweets.extend(result)
+
+if len(tweets) == 0:
     print('No tweets found.')
-else:
+else:    
     print('Got tweets, generating csv...')
-    fieldnames = tweets[0].__dict__.keys()
+    fieldnames = tweets[0].__dict__.keys() # get header
+
+    # write data to csv
     with open('tweets.csv', mode='w', newline='\n', encoding='utf-8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for tweet in tweets:
             writer.writerow(tweet.__dict__)
+
 print('Done!')
