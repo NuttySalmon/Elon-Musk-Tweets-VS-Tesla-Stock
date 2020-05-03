f0d3df9607b710fa98bc6e598469d0f9cad05c37
NuttySalmon <nuttysalmon@gmail.com>
2020-05-02 22:57:30 -0700

    Added script to get data for stocks.

---
+++ get_stocks.py
@@ -0,0 +1,6 @@
+import yfinance as yf
+import pandas as pd
+
+if __name__ == "__main__":
+    data = yf.download("TSLA ^IXIC", start="2016-01-01", end="2019-11-07", interval="1d")
+    data.to_pickle('tsla.pkl')
