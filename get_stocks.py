33c194ff967624cea8d797063ae3e135e82d209a
NuttySalmon <nuttysalmon@gmail.com>
2020-05-03 19:11:50 -0700

    Extended stock data.

--- get_stocks.py
+++ get_stocks.py
@@ -2,5 +2,5 @@ import yfinance as yf
 import pandas as pd
 
 if __name__ == "__main__":
-    data = yf.download("TSLA ^IXIC", start="2016-01-01", end="2019-05-02", interval="1d")
+    data = yf.download("TSLA ^IXIC", start="2016-01-01", end="2020-05-02", interval="1d", tz='EST')
     data.to_pickle('tsla.pkl')
